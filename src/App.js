76a483c7cbc3f27a612b80be738c7acd1a3c9db8
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2022-10-13 15:31:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Pricing from './components/NavBar/Pricing/Pricing';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
+import NavBar from './components/NavBar/NavBar';
+import PhoneBar from './PhoneBar/PhoneBar';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <h1 className='text-4xl font-bold underline'>This is a big Header</h1>
+      <p>This is just a Paragraph</p>
+      <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
